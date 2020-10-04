b7ed8909ecda7f09cdcfd03a205ffb83ac534421
xcgame520 <805627081@qq.com>
2020-10-04 12:58:28 +0800

    2.4.4

---
+++ src/settings.d1e58.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",resources:"0565a",main:"3c934"}};
