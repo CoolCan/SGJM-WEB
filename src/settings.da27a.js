6e23a9225bba8e534134a9eedc4a9ab1c3fb3116
xcgame520 <805627081@qq.com>
2021-02-11 13:19:48 +0800

    2.5.0.3

---
+++ src/settings.da27a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",resources:"a13a8",main:"21018"}};
