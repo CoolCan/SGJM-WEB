705286730531e2f2b841ddb7cf62a4c271d38bc1
郭灿 <805627081@qq.com>
2021-04-25 11:48:04 +0800

    v3.0.1

--- src/settings.cfac5.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",resources:"2f615",main:"0ac49"}};
