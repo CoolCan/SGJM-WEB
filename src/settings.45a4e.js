6c8d505f500036d8b5e301cc7bb482f44889d674
郭灿 <805627081@qq.com>
2021-04-28 17:38:34 +0800

    v3.0.2

---
+++ src/settings.45a4e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"efe26",resources:"dc4f6",main:"bf77d"}};
--- src/settings.fd49d.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"efe26",resources:"0669a",main:"f2263"}};
