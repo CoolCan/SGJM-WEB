4516c0131cb994320ca6fe7316537bb307b2b907
郭灿 <805627081@qq.com>
2021-07-05 17:50:27 +0800

    v3.0.6

--- src/settings.eb4be.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"803f8",resources:"361f8",main:"1a2ae"}};
