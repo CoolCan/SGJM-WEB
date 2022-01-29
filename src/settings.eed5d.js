b48f5fb50fe935e75eaeafcb98c55e6fa7d76396
xcgame520 <805627081@qq.com>
2022-01-29 23:44:11 +0800

    v3.0.0.2

--- src/settings.eed5d.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"1b082",resources:"cea78",main:"3fc11"}};
