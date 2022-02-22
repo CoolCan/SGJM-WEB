72b13a71228bd6c4bba92c3fbb5ee5f9342b3147
xcgame520 <805627081@qq.com>
2022-02-23 03:57:59 +0800

    v3.1.3

---
+++ src/settings.85768.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"1b082",resources:"7f095",main:"73934"}};
